7f8c26b030f3faeda2a368bf6bcec5ff3c15ecf5
Erik Mast <e.mast@drenthecollege.nl>
2017-11-06 17:07:51 +0100

    Users toegevoegd

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,14 +11,26 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-                DB::table('users')->insert(
-                [
-                    [
-						'email' => 'e.mast@drenthecollege.nl',
+      DB::table('users')->insert(
+      [
+        [
+				      'email' => 'e.mast@drenthecollege.nl',
                         'password' => bcrypt('Test'),
                         'firstname' => 'Erik',
                         'lastname' => 'Mast'
-                    ]
-        ]);
+        ],
+        [
+              'email' => 'a.balfaqih@drenthecollege.nl',
+                        'password' => bcrypt('Test'),
+                        'firstname' => 'Aminah',
+                        'lastname' => 'Balfaqih'
+        ],
+        [
+              'email' => 'h.steenbergen@drenthecollege.nl',
+                        'password' => bcrypt('Test'),
+                        'firstname' => 'Harmen',
+                        'lastname' => 'Steenbergen'
+        ]
+      ]);
     }
 }
