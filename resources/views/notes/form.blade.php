5ad1060c682094e1885a21b5cef7042c17b86d3c
Erik Mast <e.mast@drenthecollege.nl>
2017-11-11 19:30:44 +0100

    User en Student id als hidden controls

--- resources/views/notes/form.blade.php
+++ resources/views/notes/form.blade.php
@@ -1,17 +1,11 @@
 
-<div class="row">
-        <div class="col-lg-3">{{ Form::label('students_id', 'Student: ') }}</div>
-		      <div class="col-lg-6">{!! Form::text('students_id') !!}</div>
-</div>
-<div class="row">
-        <div class="col-lg-3">{{ Form::label('users_id', 'User: ') }}</div>
-		      <div class="col-lg-6">{!! Form::text('users_id') !!}</div>
-</div>
+{!! Form::hidden('students_id', $note->Student->id) !!}
+{!! Form::hidden('users_id', $note->User->id) !!}
+
 <div class="row">
         <div class="col-lg-3">{{ Form::label('note_types_id', 'Soort notitie: ') }}</div>
         <div class="col-lg-6">{!! Form::select('note_types_id', $notetypes, $note->NoteType->id) !!}</div>
 </div>
-
 <div class="row">
         <div class="col-lg-3">{{ Form::label('notes', 'Notitie: ') }}</div>
 		<div class="col-lg-6">{!! Form::text('notes') !!}</div>
