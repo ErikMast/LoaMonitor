e94dc624e701b931888a773cc8168d9111a0c05c
Erik Mast <e.mast@drenthecollege.nl>
2017-11-11 18:45:59 +0100

    Bug bij index van alle studenten

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -11,7 +11,11 @@
         		    @endif
             </div>
             <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('notes.create', ['student_id' => $student->id, 'user_id'=>Auth::user()->id]) }}"> Nieuwe notitie</a>
+                @if (!$allStudents)
+                  <a class="btn btn-success" href="{{ route('notes.create', ['student_id' => $student->id, 'user_id'=>Auth::user()->id]) }}"> Nieuwe notitie</a>
+                @else
+                  <a class="btn btn-success" href="{{ route('notes.create', ['user_id'=>Auth::user()->id]) }}"> Nieuwe notitie</a>
+                @endif
             </div>
         </div>
     </div>
