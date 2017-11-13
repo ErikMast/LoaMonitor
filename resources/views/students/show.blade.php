e04cb1379c35522c5619a43ffd45d102a6eb91d9
Erik Mast <e.mast@drenthecollege.nl>
2017-11-13 17:14:37 +0100

    Notities etc overzicht en knoppen op overzicht student scherm

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -24,13 +24,58 @@
     </div>
 
 	<div class="row">
-        <div class="col-lg-6">{{ Form::label('firstname', 'Voornaam: ') }}</div>
-		<div class="col-lg-6">{{ $student->firstname }}</div>
+        <div class="col-lg-3">{{ Form::label('naam', 'Naam: ') }}</div>
+		<div class="col-lg-6">{{ $student->firstname }} {{ $student->lastname }}</div>
 	</div>
 	<div class="row">
-        <div class="col-lg-6">{{ Form::label('lastname', 'Achternaam: ') }}</div>
-        <div class="col-lg-6">{{ $student->lastname }}</div>
+        <div class="col-lg-3">{{ Form::label('student_number', 'Stamnr: ') }}</div>
+        <div class="col-lg-6">{{ $student->student_number }}</div>
     </div>
+		<div class="row">
+	        <div class="col-lg-3">{{ Form::label('group', 'Klas: ') }}</div>
+	        <div class="col-lg-6">{{ $student->Group->name }}</div>
+	    </div>
+	<div class="row">
+        <div class="col-lg-3">{{ Form::label('village', 'Woonplaats - ETA: ') }}</div>
+		<div class="col-lg-6">{{ $student->Village->name }} ({{ $student->eta }})</div>
+	</div>
+
+	<table class="display table table-bordered table-condensed table-responsive dynamic-table">
+	  <thead>
+	    <tr>
+	      <th>Notitie</th>
+	      <th width="300px">Modules</th>
+	      <th width="300px">Acties</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	    <br>
+	      <tr class="clickable-row" data-url="/student/{{ $student->id }}">
+	        <td>
+	          @foreach($student->notes as $note)
+	          <strong>{{$note->NoteType->name}}</strong>
+	          {{$note->date}} {{$note->notes}}<br>
+	          @endforeach
+	        </td>
+	        <td>
+	          Modules komen hier
+	        </td>
+	        <td>
+	          <a href="{{ url('/students/' . $student->id ) }}">
+	            <button class="btn btn-info">
+	              <span class="glyphicon glyphicon-info-sign"> Info</span>
+	            </button>
+	          </a>
+	          <a href="{{ route('notes.create', ['student_id' => $student->id, 'user_id'=>Auth::user()->id])}}">
+	            <button class="btn btn-warning">
+	              <span class="glyphicon glyphicon-plus"> Notitie</span>
+	            </button>
+	          </a>
+	        </td>
+	      </tr>
+	  </tbody>
+	</table>
+
 	{!! Form::close() !!}
 </div>
 
